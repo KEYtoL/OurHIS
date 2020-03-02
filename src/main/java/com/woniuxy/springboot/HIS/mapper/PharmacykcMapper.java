1a8cec43e14a38eafab3cb325740ddeb1994e018
lg <lg@163.com>
2020-03-02 20:13:57 +0800

    库存表over

--- src/main/java/com/woniuxy/springboot/HIS/mapper/PharmacykcMapper.java
+++ src/main/java/com/woniuxy/springboot/HIS/mapper/PharmacykcMapper.java
@@ -15,10 +15,16 @@ public interface PharmacykcMapper {
 		Pharmacykc selectPharmacykcByMid(Integer mid);
 		//按查询所有库存
 		List<Pharmacykc> selectAllPharmacykc();
+		//查询不足库存
+		List<Pharmacykc> selectPharmacykcByYfkcnum();
 		//修改库存数量通过mid
 		void updatePharmacykcByMid
 		(@Param("yfkcnum") Integer yfkcnum,@Param("mid")Integer mid);
 		//删除库存通过mid
 		void deletePharmacykcByMid(Integer mid);
+		//通过名字查询，用于分页
+		List<Pharmacykc> selectPharmacykcByYfkcnumAndMname(String mname);
+		//通过名字查全部
+		List<Pharmacykc> selectPharmacykcByMname(String mname);
 		
 }
